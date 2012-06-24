b52747334dcba9c143ec9c64d03f8d8c4f06892c
Cesar Luis <cesardluis@gmail.com>
2012-06-23 21:35:06 -0300

    finish multi select

--- js/simple.selector.js
+++ js/simple.selector.js
@@ -16,7 +16,7 @@
             this.id = this.$elem.attr('id');
             this.type = (this.$elem.attr("multiple") == undefined) ? 'single': 'mult';
             this.width = (parseInt(this.$elem.outerWidth()) > 0) ? parseInt(this.$elem.outerWidth()): this.config.width;
-            //this.$elem.css('display', 'none');
+            this.$elem.css('display', 'none');
 
             if (this.type == 'single')
                 this.single();
@@ -24,17 +24,17 @@
                 this.multi();           
         },
         single: function(){
-            jss = this;
-            var $tplSingle = $('<div class="jss_wrap jss_single" id="jss_'+this.id+'" style="width: '+this.width+'px">'
-                +'<div class="jss_box"  style="width: '+(this.width - 2)+'px"><div class="jss_item"></div><span class="jss_arrow"></span></div>'
-                +'<ul class="jss_options" style="width: '+(this.width - 2)+'px; display:none; z-index: '+ jss.zIndex()+'"></ul></div>');
-            var options = this.getOptions();
+            var jss = this;
+            var $tplSingle = $('<div class="jss_wrap jss_single" id="jss_'+jss.id+'" style="width: '+jss.width+'px">'
+                +'<div class="jss_box"  style="width: '+(jss.width - 2)+'px"><div class="jss_item"></div><span class="jss_arrow"></span></div>'
+                +'<ul class="jss_options" style="width: '+(jss.width - 2)+'px; display:none; z-index: '+ jss.zIndex()+'"></ul></div>');
+            var options = jss.getOptions();
             if (options.length>0) {
                 for(i in options){
                     var $optionsLi = $('<li>'+options[i].text+'</li>');
-                    if (i==0 || options[i].value == this.$elem.val())
+                    if (i==0 || options[i].value == jss.$elem.val())
                         $(".jss_item", $tplSingle).html(options[i].text);
-                    if (options[i].value == this.$elem.val())
+                    if (options[i].value == jss.$elem.val())
                         $optionsLi.addClass('jss_active');
 
                     $optionsLi.data("value",options[i].value).click(function(){
@@ -58,53 +58,61 @@
             });
 
             $('.jss_item, .jss_arrow, li', $tplSingle).click(function(){
-                if(!$('.jss_arrow', $tplSingle).hasClass('jss_arrow_hover')){
-                    $('.jss_arrow', $tplSingle).addClass('jss_arrow_hover');
+                if(!$('.jss_arrow', $tplSingle).hasClass('jss_arrow_active')){
+                    $('.jss_arrow', $tplSingle).addClass('jss_arrow_active');
                     $('.jss_box', $tplSingle).addClass('jss_active');
                     $(".jss_options", $tplSingle).show();
                 }else{
-                    $('.jss_arrow', $tplSingle).removeClass('jss_arrow_hover');
+                    $('.jss_arrow', $tplSingle).removeClass('jss_arrow_active');
                     $('.jss_box', $tplSingle).removeClass('jss_active');
                     $(".jss_options", $tplSingle).hide();
 
                 }
             });
-            this.$elem.after($tplSingle);
+            jss.$elem.after($tplSingle);
         },
         multi: function(){
-            jss = this;
-            var $tplMulti = $('<div class="jss_wrap jss_mult" id="jss_'+this.id+'" style="width: '+this.width+'px">'
-                +'<div class="jss_box"  style="width: '+(this.width - 2)+'px"><input type="text" class="jss_search"></div>'
-                +'<ul class="jss_options" style="width: '+(this.width - 2)+'px; display:none; z-index: '+ jss.zIndex()+'"></ul></div>');
-            var options = this.getOptions();
+            var jss = this;
+            var $tplMulti = $('<div class="jss_wrap jss_mult" id="jss_'+jss.id+'" style="width: '+jss.width+'px">'
+                +'<div class="jss_box"  style="width: '+(jss.width - 8)+'px"></div>'
+                +'<ul class="jss_options" style="width: '+(jss.width - 2)+'px; display:none; z-index: '+ jss.zIndex()+'"></ul></div>');
+            var options = jss.getOptions();
 
             var addItem = function(elem, text, value){
                 var $item = $('<div class="jss_item">'+ text +'<span class="jss_delete"></span></div>');
-                $('.jss_delete',$item).data("value", value).click(function(){
-                    $(this).parent().remove();
-                    var values = (jss.$elem.val() != null) ? jss.$elem.val(): [];
-                    var pos = values.lastIndexOf($(this).data('value'));
-                    values.splice(pos,1);
-                    jss.$elem.val(values);
-                    if (values.length <=0) 
-                        $('.jss_box', $tplMulti).height('25px');
-                });
-                $('.jss_search', $tplMulti).before($item);
+                $('.jss_delete',$item).data("value", value);
                 $('.jss_box', $tplMulti).height('auto');
-            }
 
+                $('.jss_delete',$item).click(function(){
+                    var values = (jss.$elem.val() != null) ? jss.$elem.val(): [];
+                    var val = $(this).data('value');
+                    for (i in values)
+                        if( val  === values[i])
+                            values.splice(i,1);
+                    if (values.length === 0)
+                        $('.jss_box', $tplMulti).height('25px'); 
+
+                    jss.$elem.val(values);
+                    $('li', $tplMulti).each(function(){
+                        var active = $(this).data("value");
+                        if(val == active)
+                            $(this).removeClass('jss_active');
+                    });
+                    $(this).parent().remove();
+                });
+                //console.log(values);
+                $('.jss_box', $tplMulti).append($item);
+                
+            }
             if (options.length>0) {
                 for(i in options){
                     var $optionsLi = $('<li>'+options[i].text+'</li>');
                     $optionsLi.data("value",options[i].value);
                     var selects = (jss.$elem.val() != null) ? jss.$elem.val(): [];
-                    if (selects.length <=0) 
-                        $('.jss_box', $tplMulti).height('25px');
-
                     for (var j = 0; j < selects.length; j++) {                       
                         if (options[i].value == selects[j]){
                             $optionsLi.addClass('jss_active');
-                            addItem(this, options[i].text, options[i].value);
+                            addItem($optionsLi, options[i].text, options[i].value);
                         }
                     }                    
                     $optionsLi.click(function(){
@@ -128,24 +136,17 @@
                 $(this).removeClass("jss_box_hover");
             });
 
-            $('.jss_box, li', $tplMulti).click(function(){
-                $(".jss_options", $tplMulti).show();
-                $(".jss_search", $tplMulti).focus();
-
-                if(!$('.jss_box', $tplMulti).hasClass('jss_active')){
-                    $('.jss_box', $tplMulti).addClass('jss_active');
+            $('.jss_box', $tplMulti).click(function(){
+                if(!$(this).hasClass('jss_active')){
+                    $(this).addClass('jss_active');
                     $(".jss_options", $tplMulti).show();
                 }else{
-                    $('.jss_box', $tplMulti).removeClass('jss_active');
+                    $(this).removeClass('jss_active');
                     $(".jss_options", $tplMulti).hide();
-                    jss.$elem.focus();
-
                 }
             });
 
-            this.$elem.after($tplMulti);
-
-            
+            jss.$elem.after($tplMulti);
 
         },
         zIndex: function(){
@@ -161,20 +162,6 @@
                 options.push({pos: i, value: $item.val(), text: $item.text()});
             });
             return (options)
-        },
-        strTo: function(str){
-            var e = "áéíóú";
-            var r = "aeiou";
-            str = str.toLowerCase();
-            for (var i = 0; i < e.length; i++) {
-                str = this.replace(e[i],r[i], str);
-            };
-            return str;
-        },
-        replace: function(search, replace, string){
-            while (string.toString().indexOf(search) != -1)
-                string = string.toString().replace(search,replace);
-            return string;
         }
     }
     $.fn.jssimple = function(opciones) {
